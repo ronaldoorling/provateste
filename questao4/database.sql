76a8235f6167077aa7c0fe9e5231798a075e6793
ronaldoorling <ronaldo.orling@gmail.com>
2015-09-19 01:12:13 -0300

    Arquivo Database

---
+++ questao4/database.sql
@@ -0,0 +1,13 @@
+﻿create database teste;
+use teste;
+
+CREATE TABLE  tarefas (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  tarefa varchar(100) NOT NULL,
+  descricao text,
+  prioridade int(11) DEFAULT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+insert into tarefas VALUES (1, 'Tarefa 1', 'Descrição da Tarefa 1', 1);
+insert into tarefas VALUES (2, 'Tarefa 2', 'Descrição da Tarefa 2', 2);
